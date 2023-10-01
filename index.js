0b1a7b02d296d40bb2eb297202480c23275bf5a1
Hadeel Obaid <hadeelobaid71@gmail.com>
2023-10-02 02:10:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+
+function mondayWork(onlineOrNot = "go to the office") {
+return `This Monday, I will ${onlineOrNot}.`;
+}
+
+function wrapAdjective(me="*") {
+    return function(youAre = "special") {
+        return `You are ${me}${youAre}${me}!`
+    }
+}
